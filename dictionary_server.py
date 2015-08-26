f3f89d01dcb4128d3e1ceabcf1ad20ba024fa4ef
bsloan666 <bsloan@pixelmonks.com>
2015-08-25 22:50:24 -0700

    added my app, removed sample

---
+++ dictionary_server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import re 
+from functools import wraps
+import sys
+import json
+import threading
+import time
+import yaml
+import cgi
+
+from dd.runtime import api
+api.load('flask')
+from flask import Flask
+from flask import make_response
+from flask import request 
+import generate
+
+abort = False
+
+app = Flask(__name__)
+
+@app.route('/urbandictionaryphrases')
+def toplevel():
+    root = generate.random_phrase()
+    head="""
+        <html>
+        <head>
+        <style>
+        h2 {text-align:center;}
+        p {text-align:center;}
+        </style>
+        </head>
+        <body><br><br><br><br><br><h2><font face=arial>"""
+    foot="""
+        </font></h2></body>
+        </html>"""
+    resp = make_response("%s%s%s"%(head,cgi.escape(root),foot))
+    resp.headers['Content-Type'] = 'text/html'
+    resp.headers['access-control-allow-origin'] = '*'
+    return resp 
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", debug=True, use_reloader=False)
+
---
+++ generate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import os
+from random import shuffle
+
+def init_db(fname):
+    handle = open(fname, 'r')
+    lines = handle.readlines()
+    handle.close()
+    return lines
+
+def random_phrase():
+    adjs = init_db('adjectives.db')
+    nouns = init_db('nouns.db')
+
+    shuffle(adjs)
+    shuffle(nouns)
+
+    a_range = range(len(adjs))
+    n_range = range(len(nouns))
+
+    shuffle(a_range)
+    shuffle(n_range)
+
+    return "%s %s"%(adjs[a_range[0]].strip(), nouns[n_range[0]].strip())
+
+if __name__ in "__main__":
+    print random_phrase()
+
