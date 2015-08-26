20021240c757dd6727f9f59a9b7fdf493ee43599
bsloan666 <bsloan@pixelmonks.com>
2015-08-25 23:12:25 -0700

    duh RTFM

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn dictionary_server:app --log-file -
--- dictionary_server.py
+++ dictionary_server.py
@@ -42,5 +42,5 @@ def toplevel():
     return resp 
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", debug=True, use_reloader=False)
+    app.run()
 
