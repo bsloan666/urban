91e2beb87e946fd5f8a3859906e7bdf9cb95948b
Brandon Ashworth <brandon@brandonashworth.com>
2015-09-12 19:14:50 -0700

    Updated the hall of fame template and gave the hall of fame its own route. Also changed a few urls to use relative urls instead of absolute.

--- hall_of_fame.py
+++ hall_of_fame.py
@@ -1,8 +1,10 @@
 import os
 import sys
 import re
+import urlparse
 
 from random import Random
+
 
 def parse_url(url):
     data = {}
@@ -12,8 +14,9 @@ def parse_url(url):
     for keyval in keyvalarray:
         (key,val) = re.split("=", keyval)
         data[key] = val
-    
+
     return data
+
 
 def random_color():
     r = Random()
@@ -26,19 +29,24 @@ def random_color():
 def table(contents):
    return '<table align="center" border="0" cellpadding="10">\n'+contents+'\n</table>\n'
 
+
 def row(contents):
     return '<tr>'+contents+'</tr>\n'
+
 
 def column(contents, color):
     return '<td align=center bgcolor="'+color+'">'+contents+'</td>'
 
+
 def img(adj,noun,link):
-    return '<img align=center height="128" src="'+link+'" alt="'+adj+' '+noun+'"/>' 
+    return '<img align=center height="128" src="'+link+'" alt="'+adj+' '+noun+'"/>'
+
 
 def link(adj,noun,img, link):
     adj = re.sub('\+',' ',adj)
     noun = re.sub('\+',' ',noun)
     return '<a href="'+link+'"><b>'+adj+' '+noun+'</b><br/><p>'+img+'</p><br/><br/></a>'
+
 
 def load_faves():
     handle = open('favorites.db','r')
@@ -46,15 +54,26 @@ def load_faves():
     handle.close()
     return lines
 
+
+def process_faves(fav_lines):
+    faves = []
+    for fav in fav_lines:
+        url_data = urlparse.urlparse(fav.strip())
+        parsed_url = urlparse.parse_qs(url_data.query)
+        parsed_url['url'] = fav.strip()
+        faves.append(parsed_url)
+    return list(reversed(faves))
+
+
 def build_table():
     faves = load_faves()
-    myrow = "" 
+    myrow = ""
     contents = ""
     columns = 0
     for fave in faves[::-1]:
         data = parse_url(fave)
-        color = '#FFFFFF' 
-        myrow += column(link(data['adj'], data['noun'], 
+        color = '#FFFFFF'
+        myrow += column(link(data['adj'], data['noun'],
             img(data['adj'], data['noun'],data['imgurl']), fave), color)
         columns = columns+1
         if columns >= 3:
@@ -63,4 +82,3 @@ def build_table():
             myrow = ""
 
     return table(contents)
-
